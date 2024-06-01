16e1d04a1373751709e0fb36567642ced920d72a
fajriiksn <154760269+fajriiksn@users.noreply.github.com>
2024-06-01 13:29:22 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:foresthreebkt@gmail.com">foresthreebkt@gmail.com</a>
+         <a href="mailto:csforest@gmail.com">csforestt@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>10:00am to 22:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">JL.H.Burhan, Bukittinggi </a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:0812-7593-5698">0812-7593-5698</a>
+         <a href="tel:0812-7593-5698">0812-7593-5698</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright foresthreebkt <?= date('Y'); ?> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
